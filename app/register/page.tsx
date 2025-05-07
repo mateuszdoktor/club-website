2375bc4aa55aaef3551931a4bd19661a75370b1c
Mateusz Doktor <matdoktor3@gmail.com>
2025-05-07 11:58:06 +0200

    fix login and register wrapped with suspense boundary

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import { RegisterForm } from "@/components/auth/RegisterForm";
 
 export default function RegisterPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <RegisterForm />
+      <Suspense fallback={<div>Loading register...</div>}>
+        <RegisterForm />
+      </Suspense>
     </div>
   );
 }
