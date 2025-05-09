2041357d529895880e2de017da86e7ce76f04446
Mateusz Doktor <matdoktor3@gmail.com>
2025-05-09 16:20:47 +0200

    Hero image preload

--- components/layout/header/Header.jsx
+++ components/layout/header/Header.jsx
@@ -1,11 +1,19 @@
+"use client";
+
+import Image from "next/image";
 import Navbar from "./navbar/Navbar";
 import HeroText from "./HeroText";
+
 export default function Header() {
   return (
-    <header
-      className="relative h-screen w-full bg-cover bg-center"
-      style={{ backgroundImage: "url(/IMG_2739.JPG)" }}
-    >
+    <header className="relative h-screen w-full overflow-hidden">
+      <Image
+        src="/IMG_2739.JPG"
+        alt=""
+        fill
+        priority
+        className="object-cover object-center z-0"
+      />
       <div className="absolute inset-0 bg-gradient-to-b from-black/80 via-black/50 to-black/20 z-10" />
       <Navbar />
       <HeroText />
